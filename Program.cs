2256167292f0286a3d41928c564bbf7de389f4c4
Emily Morgenbesser <emorgenbesser@hedgecovest.local>
2023-05-09 14:42:17 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Console.WriteLine("Emily is great");
+int age = 26;
+
+string name = "Emily";
+
+bool isHere = true;
+
+double salary = 22000.01;
+
+Console.WriteLine($"My name is {name} and my age is {age}? {isHere}");
+Console.WriteLine($"And you make {salary} a year");
+
